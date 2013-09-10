d4dc0ceca096b73e4464162bddff9299be6f2268
SaschaNaz <saschanaz@outlook.com>
2013-09-10 22:00:03 +0900

    SRT index starts with 1

--- SamiTS/subripwriter.js
+++ SamiTS/subripwriter.js
@@ -12,7 +12,7 @@ var SamiTS;
                 subDocument += "\r\n" + text;
             };
             var text;
-            var syncindex = 0;
+            var syncindex = 1;
             if (xsyncs.length > 0) {
                 text = xsyncs[0].innerText.trim();
                 if (text.length > 0)
